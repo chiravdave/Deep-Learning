7a3fdc0f4912e3f5a8710f0bf0ff2c4e66869502
Chirav Dave <davechirav@gmail.com>
2018-08-19 18:27:19 -0700

    MNIST_DNN
    Accuracy is ~98% with Adam Optimizer and ~92% for SGD

--- Practice/mnist.py
+++ Practice/mnist.py
@@ -1,78 +1,76 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-# Imports
-import numpy as np
 import tensorflow as tf
+import numpy as np
 
-tf.logging.set_verbosity(tf.logging.INFO)
+def conv_2Dlayers(inputs, filters, kernel_size, stride, pad, non_linearity):
+  return tf.layers.conv2d(inputs, filters, kernel_size, strides = stride, padding = pad, activation = non_linearity)
 
+def pool_2Dlayer(inputs, pooling_size, stride, pad):
+  return tf.layers.max_pooling2d(inputs, pooling_size, strides = stride, padding = pad)
 
-# Our application logic will be added here
-def mnist_model(features, labels, mode):
-    # Input Layer
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])  # Reshape input into [bacth_size, width, height, n_channels]
+def dense_layer(inputs, units, non_linearity):
+  return tf.layers.dense(inputs, units, activation = non_linearity)
 
-    # Convolutional Layer 1
-    conv_1 = tf.layers.conv2d(inputs=input_layer, filters=32, kernel_size=5, padding="same", activation=tf.nn.relu)
+def dropout_layer(inputs, drop_rate, train):
+  return tf.layers.dropout(inputs, rate = drop_rate, training = train)
 
-    # Pooling Layer 1
-    pool_1 = tf.layers.max_pooling2d(inputs=conv_1, pool_size=2, strides=2)
+def my_network(features, mode):
+  input_layer =  tf.reshape(features, [-1,28,28,1])
+  #Convolutional Layer 1
+  conv1 = conv_2Dlayers(input_layer, 32, 5, 1, 'same', tf.nn.relu)
+  #pooling layer 1
+  pool1 = pool_2Dlayer(conv1, 2, 2, 'valid')
+  #Convolutional Layer 2
+  conv2 = conv_2Dlayers(pool1, 64, 5, 1, 'same', tf.nn.relu)
+  #pooling layer 2
+  pool2 = pool_2Dlayer(conv2, 2, 2, 'valid')
+  #Flatten all the feature maps for the dense layer
+  flat = tf.reshape(pool2, [-1, 7*7*64])
+  #Dense layer
+  dense = dense_layer(flat, 1024, tf.nn.relu)
+  #Dropout after dense layer
+  dropout = dropout_layer(dense, 0.4, mode)
+  #Logits dense layer
+  logits = dense_layer(dropout, 10, None)
+  return logits
 
-    # Convolutional Layer 2
-    conv_2 = tf.layers.conv2d(inputs=pool_1, filters=64, kernel_size=5, padding="same", activation=tf.nn.relu)
+def train_test():
+  # Load training and testing data
+  mnist_data = tf.contrib.learn.datasets.load_dataset('mnist')
+  features = tf.placeholder(shape=(None, 784), dtype=tf.float32)
+  labels = tf.placeholder(shape=(None), dtype=tf.int64)
+  mode = tf.placeholder(shape=(), dtype=tf.bool)
+  logits = my_network(features, mode)
+ 
+  #Finding the prediction label
+  label_pred = tf.argmax(logits, axis=1)
 
-    # Pooling Layer 2
-    pool_2 = tf.layers.max_pooling2d(inputs=conv_2, pool_size=2, strides=2)
+  #Finding probabilities of the predicted labels
+  label_prob = tf.nn.softmax(logits)
 
-    # Dense Layer 1
-    flatten = tf.reshape(pool_2, [-1, 3136])  # 7X7X64 = 3136
-    dense_1 = tf.layers.dense(inputs=flatten, units=1024, activation=tf.nn.relu)
-    dropout = tf.layers.dropout(inputs=dense_1, rate=0.4, training=(mode == tf.estimator.ModeKeys.TRAIN))
+  #Calulate loss function
+  one_hot_label = tf.one_hot(labels, depth = 10, dtype=tf.int64) # Depth = No.of Classes
+  loss = tf.losses.softmax_cross_entropy(one_hot_label, label_prob)
 
-    # Final Dense Layer
-    logits = tf.layers.dense(inputs=dropout, units=10)
-    prediction = {"classes": tf.argmax(input=logits, axis=1), "probabilities": tf.nn.softmax(logits, name="softmax_tensor")}
+  #Optimize the weigths and decrease the loss incurred
+  #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+  optimizer = tf.train.AdamOptimizer()
+  model = optimizer.minimize(loss)
 
-    # Testing
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=prediction)
+  #Evaluate the accuracy of the predictions by the model
+  accu_bool = tf.equal(labels, label_pred)
+  accuracy = tf.reduce_mean(tf.cast(accu_bool, tf.float32))
+  
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(30001):
+      train_data = mnist_data.train.next_batch(120) # Creating batches for training
+      loss_run, _ = sess.run((loss, model), feed_dict={features: train_data[0], labels: train_data[1], mode : True})
+      if((i+1)%500 == 0):
+        print('Loss at {} epoch is: {}'.format((i+1)/500,loss_run))
+    for i in range(1):
+      test_data = mnist_data.test.next_batch(10000) # Creating batches for testing
+      accurate = sess.run(accuracy, feed_dict={features: test_data[0], labels: test_data[1], mode : False})
+      print('Accuracy at {} iteration is: {}'.format((i+1),accurate))
 
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-    # Training
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-    # Evaluating
-    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels, predictions=prediction["classes"])}
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-
-def main(argv):
-    # Load training and eval data
-    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-    train_data = mnist.train.images
-    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-    eval_data = mnist.test.images
-    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-    # Creating the Estimator
-    mnist_classifier = tf.estimator.Estimator(model_fn=mnist_model,
-                                              model_dir="/tmp/mnist_convnet_model")  # model_dir will be used to store checkpoints
-    # Set up logging for predictions
-    tensors_to_log = {"probabilities": "softmax_tensor"}
-    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
-    # Train the model
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": train_data}, y=train_labels, batch_size=100, num_epochs=None, shuffle=True)
-    mnist_classifier.train(input_fn=train_input_fn, steps=20000, hooks=[logging_hook])
-    # Evaluate the model and print results
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": eval_data}, y=eval_labels, num_epochs=1, shuffle=False)
-    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
-    print(eval_results)
-
-
-if __name__ == "__main__":
-    tf.app.run()
+if __name__ == '__main__':
+  train_test()
