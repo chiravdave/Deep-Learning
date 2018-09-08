352d24382af317a563a0aae85005bfd673730ed0
Chirav Dave <davechirav@gmail.com>
2018-09-08 14:37:32 -0700

    Update find_phone.py

--- Finding_Phone/find_phone.py
+++ Finding_Phone/find_phone.py
@@ -1,42 +1,33 @@
 import tensorflow as tf
 import numpy as np
-from preprocess import normalized_testimages
+from preprocess import normalizeImage
 import cv2
 import sys
 
-#creating a session
-sess = tf.Session()
+def test_network(image_path):
+    #creating a session
+    sess = tf.Session()
+    #restoring my trained model
+    saver = tf.train.import_meta_graph("./model.ckpt.meta")
+    saver.restore(sess,tf.train.latest_checkpoint('./'))
+    graph = tf.get_default_graph()
+    #getting my variables from the trained model
+    x = graph.get_tensor_by_name("Placeholder:0")
+    prediction = graph.get_tensor_by_name("dense_1/BiasAdd:0")
+    mode = graph.get_tensor_by_name("Placeholder_2:0")
+    bgr_image = cv2.imread(image_path)
+    b, g, r = cv2.split(bgr_image)      #get b,g,r values of the image
+    rgb_image = cv2.merge([r,g,b])      #convert into r,g,b format
+    normalized_image = normalizeImage(rgb_image)
+    reshaped_normalized_image = np.reshape(normalized_image, (-1, 326, 490, 3))
+    #storing my results
+    feed_dict = {x:reshaped_normalized_image, mode:False}
+    center_values = sess.run(prediction, feed_dict)
+    #getting my center coordinates
+    cx = center_values[0][0]
+    cy = center_values[0][1]
+    #displaying final results
+    print (cx, cy)
 
-#restoring my trained model
-saver = tf.train.import_meta_graph("model.meta")
-saver.restore(sess,tf.train.latest_checkpoint('./'))
-graph = tf.get_default_graph()
-
-#getting my variables from the trained model
-x = graph.get_tensor_by_name("input:0")
-layer = graph.get_tensor_by_name("final:0")
-is_training = graph.get_tensor_by_name("flag:0")
-
-#reading the location of the test file
-testPath = str(sys.argv[1])
-
-img = cv2.imread(testPath)
-img_resized = cv2.resize(img, (256, 256), interpolation=cv2.INTER_LINEAR)
-img_resized = normalized_testimages(img_resized)
-#storing my results
-prediction = np.zeros(1)
-
-image_linear = np.reshape(img_resized, [-1, 196608])
-feed_dict = {x:image_linear, is_training:0}
-prediction = (sess.run(layer, feed_dict))
-
-#getting my center coordinates
-ix = prediction[0][0]
-iy = prediction[0][1]
-
-#rounding the coordinate values till 4 decimal places
-normalized_x = round(ix/256.0,4)
-normalized_y = round(iy/256.0,4)
-
-#displaying final results
-print normalized_x, normalized_y
+if __name__ == '__main__':
+    test_network(sys.argv[1])
