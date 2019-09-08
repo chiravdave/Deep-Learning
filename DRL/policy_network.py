d1a3757f4b18d7b92c7389dfc4e513e90f590bae
davechirav <davechirav@gmail.com>
2019-09-07 19:08:35 -0700

    now implementing policy gradient

--- DRL/policy_network.py
+++ DRL/policy_network.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import gym
+import time
 from numpy.random import uniform, randint
 from memory import ReplayBuffer
 from preprocessing import preprocess_frame
@@ -12,27 +13,32 @@ def bias_initialize(shape, var_name):
 	b = tf.compat.v1.Variable(var_name, shape, tf.float32, initializer = tf.zeros_initializer)
 	return b
 
-class DQN:
+def copy_network_weights(source_network, target_network):
+	pass
+
+class PolicyNetwork:
 
 	def __init__(self, network_name):
 		with tf.variable_scope(network_name):
 			self.w1, self.b1 = weight_initializer([1600, 512], 'w1'), bias_initializer([512], 'b1')
-			self.w2, self.b2 = weight_initializer([512, 3], 'w2'), bias_initializer([3], 'b2')
+			self.w2, self.b2 = weight_initializer([512, 1], 'w2'), bias_initializer([1], 'b2')
 
 	def forward(self, X, network_name):
 		with tf.variable_scope(network_name):
 			h1 = tf.matmul(X, self.w1, name='h1') + self.b1
 			a1 = tf.nn.relu(h1, name='a1')
-			q_values = tf.matmul(a1, self.w2, name='q_values') + self.b2
-			return q_values
+			h2 = tf.matmul(a1, self.w2, name='h2') + self.b2
+			policy = tf.nn.sigmoid(h2, name='policy')
+			return policy
 
 class Pong:
 
 	def __init__(self):
 		self.env = gym.make('Pong-v0')
+		self.action_mapping = {'up': 2, 'down': 3, 'stay': 0}
 
 	def reset(self):
-		self.env.reset()
+		return self.env.reset()
 
 	def show(self):
 		self.env.render()
@@ -45,50 +51,51 @@ class Pong:
 		next_state, reward, is_done, _ = self.env.step(action)
 		return next_state, reward, is_done
 
-	def get_all_actions(self):
-		return self.env.action_space
+	def get_action_mapping(self):
+		return self.action_mapping
 
 def train(episodes, max_game_points):
 	episode = 1
 	replay = ReplayBuffer()
 	epsilon = 1
+	learn_rate = 0.001
 	epsilon_decay = 0.99
 	discount = 0.9
 
 	X = tf.placeholder(shape=(None, 1600), dtype=tf.float32)
-	Y = tf.placeholder(shape=(None, 3), dtype=tf.float32)
+	Y = tf.placeholder(shape=(None, 1), dtype=tf.float32)
 
-	cur_model, target_model = DQN('cur_model'), DQN('target_model')
-	current_q_values, target_q_values = cur_model.forward(X, 'cur_model'), target_model.forward(X, 'target_model')
-	loss = tf.losses.softmax_cross_entropy(Y, current_q_values)
-	optimizer = tf.train.AdamOptimizer()
-	model = optimizer.minimize(loss)
+	cur_model, target_model = PolicyNetwork('cur_model'), PolicyNetwork('target_model')
+	cur_policy, target_policy = cur_model.forward(X, 'cur_model'), target_model.forward(X, 'target_model')
+	with tf.name_scope('cur_model'):
+		loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=cur_policy, labels=Y, name='loss')
+		optimizer = tf.train.AdamOptimizer(learning_rate=learn_rate, name='adam')
+		model = optimizer.minimize(loss)
 
 	with tf.compat.v1.Session() as sess:
 		# First we have to initialize all the variables present in our computational graph
 		sess.run(tf.compat.v1.global_variables_initializer())
+		game = Pong()
 		while episode <= episodes:
 			game_point = 0
+			cur_state = preprocess_frame(game.reset())
+			prev_state = None
 			while game_point < max_game_points:
-				prev_state, cur_state, next_state = None, None, None
+				game.show()
+				x = cur_state - prev_state if prev_state else np.zeros(80*80)
+				prev_state = np.copy(x)
 				if uniform() < epsilon:
-					action = randint(3)
+					action = randint(low=2, high=4)
 				else:
-					img = cv2.imread('cur_state.jpg')
-					cur_state = preprocess_frame(cur_state.reshape((-1, 1600)), prev_state)
-					q_values = sess.run(target_q_values, feed_dict = {X : img.reshape(-1, 1600)})
-					action = tf.argmax(q_values, axis=1)
+					policy = sess.run(cur_policy, feed_dict = {X : x.reshape(-1, 1600)})
+					action = 2 if policy >= 0.5 else 3
 				next_state, reward, end = game.play(action)
-				replay.add_to_memory(cur_state, action, next_state, reward, end)
+				pass
+				replay.add_to_memory(x, action, next_state, reward, end)
 				if end == 1:
 					time += 1
 
 			# Training
 
 if __name__ == '__main__':
-	pong = Pong()
-	pong.show()
-	print(pong.get_all_actions())
-	raw_input()
-	pong.close()
-	#train(10, 10)
+	train(10, 10)
--- DRL/preprocessing.py
+++ DRL/preprocessing.py
@@ -1,8 +1,10 @@
 import numpy as np
+import cv2
 
-def preprocess_frame(img, prev_img):
+def preprocess_frame(img):
 	"""
 	This function will preprocess the input frame for DQN
 	"""
 
-	pass
+	gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	crop = gray_scale[:, :]
